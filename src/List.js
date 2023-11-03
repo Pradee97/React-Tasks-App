7e9bac514e06ca808509ab1bd538f86db0201beb
pradeepa <pradeepaa1107@gmail.com>
2023-11-03 21:24:13 +0530

    changes

--- src/List.js
+++ src/List.js
@@ -2,26 +2,26 @@ import React from 'react';
 import { Table } from 'react-bootstrap';
 
 const List = ({ beers }) => {
-  return (
-    <Table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Tagline</th>
-          {/* Add more table headers as needed */}
-        </tr>
-      </thead>
-      <tbody>
-        {beers.map((beer) => (
-          <tr key={beer.id}>
-            <td>{beer.name}</td>
-            <td>{beer.tagline}</td>
-            {/* Add more table data as needed */}
-          </tr>
-        ))}
-      </tbody>
-    </Table>
-  );
+    return (
+        <Table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Tagline</th>
+                    {/* Add more table headers as needed */}
+                </tr>
+            </thead>
+            <tbody>
+                {beers.map((beer) => (
+                    <tr key={beer.id}>
+                        <td>{beer.name}</td>
+                        <td>{beer.tagline}</td>
+                        {/* Add more table data as needed */}
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    );
 };
 
 export default List;
